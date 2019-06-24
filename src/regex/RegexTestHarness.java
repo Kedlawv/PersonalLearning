72d9d31daf18f97d0705f456d258950520b3f2f3
Kedlawv <waldek.bus@gmail.com>
2019-06-24 12:53:52 +0200

    RegexTestHarness from Java tut refactored to use Scanner instead of Console, don't understand Console didn't run from intelliJ

---
+++ src/regex/RegexTestHarness.java
@@ -0,0 +1,43 @@
+package regex;
+
+import java.io.Console;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegexTestHarness {
+
+    public static void main(String[] args){
+//        Console console = System.console();
+//        if (console == null) {
+//            System.err.println("No console.");
+//            System.exit(1);
+//        }
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("\nEnter your regex: ");
+            Pattern pattern =
+                    Pattern.compile(scanner.next());
+
+            System.out.println("\nEnter input string to search: ");
+            Matcher matcher =
+                    pattern.matcher(scanner.next());
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.printf("I found the text" +
+                                " \"%s\" starting at " +
+                                "index %d and ending at index %d.%n",
+                        matcher.group(),
+                        matcher.start(),
+                        matcher.end());
+                found = true;
+            }
+            if(!found){
+                System.out.printf("No match found.%n");
+            }
+        }
+    }
+}
